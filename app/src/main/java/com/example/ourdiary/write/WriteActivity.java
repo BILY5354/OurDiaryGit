6a2319da18c440b592b0be334fa1cd2b0a636fad
WoodPecker <2587547190@qq.com>
2022-04-29 18:05:22 +0800

    完成markdwon引入

--- app/src/main/java/com/example/ourdiary/write/WriteActivity.java
+++ app/src/main/java/com/example/ourdiary/write/WriteActivity.java
@@ -7,11 +7,10 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.ourdiary.R;
-import com.example.ourdiary.remote.data.model.Article;
 import com.example.ourdiary.remote.data.model.Tag;
-
+import com.zzhoujay.richtext.RichText;
 import java.util.List;
-import java.util.Locale;
+
 
 /**
  * 浏览博客与写博客的页面为公用的
@@ -49,7 +48,7 @@ public class WriteActivity extends AppCompatActivity {
         //如果有数据则赋值 虽然传递的是一个list 但是该list只有一个数据 所以下标为0
         if (id != -1) {
             TV_write_title.setText(title);
-            TV_write_content.setText(content);
+            RichText.fromMarkdown(content).into(TV_write_content);
         }
     }
 
